8a048222c084a6c447a978b62187700e221c531b
Vlad <maykovlad>
2019-05-21 23:03:20 +0300

    changed types of variable to Atomic

--- HomeWork1/src/Answer.java
+++ HomeWork1/src/Answer.java
@@ -4,56 +4,56 @@ import java.util.Arrays;
  * Created by MacUser on 21.05.19.
  */
 public class Answer {
-    private static double cPlus;
-    private static double java;
-    private static double js;
-    private static double cSharp;
-    private double[] val = new double[4];
+    private static AtomicInteger cPlus;
+    private static AtomicInteger java;
+    private static AtomicInteger js;
+    private static AtomicInteger cSharp;
+    private AtomicDouble[] val = new AtomicDouble[4];
 
     public Answer() {
     }
 
-    public static double getcPlus() {
+    public static AtomicInteger getcPlus() {
         return cPlus;
     }
 
-    public static void setcPlus(double cPlus) {
+    public static void setcPlus(AtomicInteger cPlus) {
         Answer.cPlus = cPlus;
     }
 
-    public static double getJava() {
+    public static AtomicInteger getJava() {
         return java;
     }
 
-    public static void setJava(double java) {
+    public static void setJava(AtomicInteger java) {
         Answer.java = java;
     }
 
-    public static double getJs() {
+    public static AtomicInteger getJs() {
         return js;
     }
 
-    public static void setJs(double js) {
+    public static void setJs(AtomicInteger js) {
         Answer.js = js;
     }
 
-    public static double getcSharp() {
+    public static AtomicInteger getcSharp() {
         return cSharp;
     }
 
-    public static void setcSharp(double cSharp) {
+    public static void setcSharp(AtomicInteger cSharp) {
         Answer.cSharp = cSharp;
     }
 
-    public double[] getVal() {
+    public AtomicDouble[] getVal() {
         return val;
     }
 
-    public void setVal(double[] val) {
+    public void setVal(AtomicDouble[] val) {
         this.val = val;
     }
 
-    public double [] statistic(){
+    public AtomicDouble [] statistic(){
         double total = getcPlus() + getcSharp() + getJava() + getJs();
         val[0] = Math.round(getcPlus() / total * 1000) / 1000.0d * 100;
         val[1] = Math.round(getJava() / total * 1000) / 1000.0d * 100;
